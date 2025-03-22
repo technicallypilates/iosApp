5607d49578ea85d5c4984ba27f9937390aa5fc3f
Patrick O'Rourke <Patrick.ORourke160893@gmail.com>
2025-03-22 20:25:00 +0000

    Initial Commit

---
+++ TechnicallyPilates/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TechnicallyPilates
+//
+//  Created by Patrick O'Rourke on 22/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
