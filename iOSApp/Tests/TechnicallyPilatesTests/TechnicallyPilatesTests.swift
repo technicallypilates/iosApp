7dcf7586438da6b452c8013b56f1af553ffc3906
Patrick O'Rourke <Patrick.ORourke160893@gmail.com>
2025-05-02 12:27:47 +0100

    Remove unused folders from iOSApp: Sounds, Sources, TechnicallyPilates, Tests

--- iOSApp/Tests/TechnicallyPilatesTests/TechnicallyPilatesTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-@testable import TechnicallyPilates
-
-final class TechnicallyPilatesTests: XCTestCase {
-    func testExample() throws {
-        // This is an example test case
-        XCTAssert(true)
-    }
-} 
